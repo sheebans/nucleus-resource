8ad57b3bebaf73bc149c4a45f828cd3429b87f2d
Ashish Mishra <ashish@gooru.org>
2015-12-14 12:26:27 +0530

    Create structure for nucleus-resource.

---
+++ src/main/java/org/gooru/nucleus/handlers/resources/bootstrap/ResourceVerticle.java
@@ -0,0 +1,18 @@
+package org.gooru.nucleus.handlers.resources.bootstrap;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.vertx.core.AbstractVerticle;
+
+public class ResourceVerticle  extends AbstractVerticle {
+  static final Logger LOG = LoggerFactory.getLogger(ResourceVerticle.class);
+
+  @Override
+  public void start() throws Exception {
+    // TODO Auto-generated method stub
+    super.start();
+  }
+
+
+}
