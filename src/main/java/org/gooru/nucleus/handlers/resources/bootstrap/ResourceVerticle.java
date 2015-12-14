5bac692ef2652a467d806b225085233fd87b19c5
Ashish Mishra <ashish@gooru.org>
2015-12-14 12:55:27 +0530

    Define an end point and provide a listener to listen there. Currently, does not do actual processing, hence kind of stubbed or dumb listener

--- src/main/java/org/gooru/nucleus/handlers/resources/bootstrap/ResourceVerticle.java
+++ src/main/java/org/gooru/nucleus/handlers/resources/bootstrap/ResourceVerticle.java
@@ -1,17 +1,26 @@
 package org.gooru.nucleus.handlers.resources.bootstrap;
 
+import org.gooru.nucleus.handlers.resources.constants.MessagebusEndpoints;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.eventbus.EventBus;
 
 public class ResourceVerticle  extends AbstractVerticle {
   static final Logger LOG = LoggerFactory.getLogger(ResourceVerticle.class);
 
   @Override
   public void start() throws Exception {
-    // TODO Auto-generated method stub
-    super.start();
+    EventBus eb = vertx.eventBus();
+
+    eb.consumer(MessagebusEndpoints.MBEP_RESOURCE, message -> {
+
+      System.out.println("Received message: " + message.body());
+      // Now send back reply
+      message.reply("Resource request received");
+    });
+    LOG.info("Resource end point ready to listen");
   }
 
 
---
+++ src/main/java/org/gooru/nucleus/handlers/resources/constants/MessagebusEndpoints.java
@@ -0,0 +1,9 @@
+package org.gooru.nucleus.handlers.resources.constants;
+
+public class MessagebusEndpoints {
+  /*
+   * Any change here in end points should be done in the gateway side as well, as both sender and receiver should be in sync
+   */
+  public static final String MBEP_RESOURCE = "org.gooru.nucleus.message.bus.resource";
+
+}
